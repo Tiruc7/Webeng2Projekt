a383e9b9dc7cbdfc1a02e9fe36ed2a246c958064
damya <damyahennige15@gmail.com>
2026-04-13 13:49:06 +0200

    added api-key, began with infrastructure for ticketmaster-queries

--- backend/src/main/java/com/notif/backend/HelloController.java
+++ backend/src/main/java/com/notif/backend/HelloController.java
@@ -1,5 +1,7 @@
 package com.notif.backend;
 
+import helper.TMQueryBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,4 +12,32 @@ public class HelloController {
     public String hello() {
         return "Backend works. Yay.";
     }
+
+    @Value("${ticketmaster.api.key}")
+    private String ticketmasterApiKey;
+
+    @GetMapping("/api/test-key")
+    public String testKey() {
+        if (ticketmasterApiKey == null || ticketmasterApiKey.isBlank()) {
+            return "Key wurde nicht geladen";
+        }
+        return "Key wurde geladen. Länge: ";
+    }
+
+    @GetMapping("/api/hello")
+    public String url() {
+        TMQueryBuilder builder = new TMQueryBuilder();
+
+        return builder.buildSearchUrl(
+                "https://app.ticketmaster.com/discovery/v2",
+                "MEIN_KEY",
+                "metallica",
+                "berlin",
+                10
+        );
+    }
+
+
+
+
 }
