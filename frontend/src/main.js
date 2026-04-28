cbe79e46ae4e3c51d3d98ade4963a0c3736140e5
Bischof, David <david.bischof@freudenberg-pm.com>
2026-04-28 09:10:02 +0200

    Added visual user recognition in frontend -> Usermenu shows which Keycloak user is currently logged in
    Added database connection to frontend
    -> Admin panel can now show a list of users from the database
    -> Admin panel can now show a list of users and concerts from the database
    -!!! Database is not connected to Keycloak management yet!!

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,12 +3,30 @@ import App from './App.vue'
 import router from './router'
 import './style.css'
 import keycloak from './keycloak/keycloak';
+import { syncAuthState } from './auth/authState'
 
+keycloak.onReady = async () => {
+  await syncAuthState()
+}
+
+keycloak.onAuthSuccess = async () => {
+  await syncAuthState()
+}
+
+keycloak.onAuthRefreshSuccess = async () => {
+  await syncAuthState()
+}
+
+keycloak.onAuthLogout = async () => {
+  await syncAuthState()
+}
 
 keycloak.init({
   onLoad: 'check-sso',
   pkceMethod: 'S256'
-}).then((_authenticated) => {
+}).then(async () => {
+  await syncAuthState()
+
   const app = createApp(App)
 
   app.config.globalProperties.$keycloak = keycloak
