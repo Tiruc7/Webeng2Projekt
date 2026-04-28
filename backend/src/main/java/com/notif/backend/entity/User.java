00636d04806d79b38accc3c1c176fa01cf65a782
kat <simonortlieb@gmx.de>
2026-04-28 13:42:32 +0200

    added repo

---
+++ backend/src/main/java/com/notif/backend/entity/User.java
@@ -0,0 +1,31 @@
+package com.notif.backend.entity;
+
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "user_name")
+    private String userName;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+}
