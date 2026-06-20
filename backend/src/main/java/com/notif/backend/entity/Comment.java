557636660c59e463603f3d6a322ed73e4fc0ba9e
Das-Nugget <simonortlieb@gmx.de>
2026-06-20 19:21:00 +0200

    Added setters for testing

--- backend/src/main/java/com/notif/backend/entity/Comment.java
+++ backend/src/main/java/com/notif/backend/entity/Comment.java
@@ -69,4 +69,8 @@ public class Comment {
     public void setContent(String content) {
         this.content = content;
     }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
 }
